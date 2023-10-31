a5fa77a43ad811691b94898a9b4a5187c197cc9d
dennis <isacsson_89@hotmail.com>
2023-10-31 10:02:59 +0100

    push

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,24 +1,41 @@
-console.log("hej från JS");
+console.log("Hej från js");
 
-let admin = false;
+
+if (JSON.parse(localStorage.getItem("admin"))) {
+  if (admin = true) {
+    console.log("admin finns");
+    let link = document.createElement("a");
+    link.innerText = "Admin";
+    link.href="/form";
+    document.body.appendChild(link);}
+  else {
+    console.log("Skapar LS");
+    localStorage.setItem("admin", JSON.stringify(false))
+  } 
+} 
+
+
 
 loginBtn.addEventListener("click", () => {
-    let user = document.getElementById("user");
-    let password = document.getElementById("password")
-    console.log("knappjävel! " + user.value + password.value)
-    if (user.innerText == admin) {
-      if(password.innerText == admin)
-      {
-        console.log("Hej admin");
-        let link = document.createElement("a");
-        link.innerText = "Admin"
-        link.href="/form"
-        document.body.appendChild(link)
-      }
-      else {
-        console.log("error");
-      }
-  }
+  let user = document.getElementById("user");
+  let password = document.getElementById("password");
+  console.log("knappjävel! " + user.value + password.value);
 
-}) 
+    if (user.value == "admin" && password.value == "admin") {
+        console.log("Snälla funka!");
+        let admin = JSON.parse(localStorage.getItem("admin"));
+        admin = true;
+        localStorage.setItem("admin", JSON.stringify(true));
+        console.log(admin);
+        let admin2 = JSON.parse(localStorage.getItem("admin"));
+        console.log(admin2);
+        let link = document.createElement("a");
+        link.innerText = "Admin";
+        link.href="/form";
+        document.body.appendChild(link);
+
+
+    }})
+
+
 
